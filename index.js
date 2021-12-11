ff2ee98bbeaf0c6876e6dd8b55486d27f7b7b476
matias acuna <agroang@gmail.com>
2021-12-11 11:30:44 +0900

    added a challenge and it's solution with some vanilla JS - working

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 )
+
+// Challenge - recreate the above line of code in vanilla JS by creating and
+// appending an h1 to our div#root(without using innerHTML).
+
+// - Create a new h1 element
+// - Give it some textContent
+// - Give it a class name of "header"
+// - append it as a child of the div#root
+
+let h1 = document.createElement("h1");
+h1.textContent = 'This is a header'
+h1.classList.add('header')
+let target = document.getElementById('root')
+target.appendChild(h1)
