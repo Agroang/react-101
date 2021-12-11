cbb285836f92bd25ee877954b65619294c3cf628
matias acuna <agroang@gmail.com>
2021-12-11 10:56:28 +0900

    putting more examples for ReactDOM.render()

---
+++ index.js
@@ -0,0 +1,8 @@
+// the first parameter is what I want to display and second is where (we can use a queryselector as well)
+ReactDOM.render(<h1>This is react baby!</h1>, document.getElementById("root"))
+
+// you can add a bunch of html as well, not just 1 element:
+// ReactDOM.render(
+//   <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+//   document.getElementById("root")
+// )
