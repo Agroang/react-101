50c3d5ed2ec2fbc8588c95bb00e777135469bd6c
matias acuna <agroang@gmail.com>
2021-12-12 16:18:25 +0900

    added a component and some comments about it

--- index.js
+++ index.js
@@ -111,3 +111,26 @@ ReactDOM.render(
 // )
 
 // ReactDOM.render(page, target)
+
+// More about React components (functions), we use them before but
+// we were using just variables until recently.
+// Conventions, function name is pascal case "LikeThis" and when you render
+// it you need to put it into < />
+
+function TemporaryName() {
+  return (
+    <div>
+      <img src="./react-logo.png" width="40px" alt="react img here" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<TemporaryName />, document.getElementById("another-div"))
